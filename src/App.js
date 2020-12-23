0ccadd2a30477429dc31b75eac190cfc9caf1b0f
stephaniecurrandev <me@stephaniecurran.dev>
2020-12-23 02:33:22 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React from 'react';
+import {getWarmup, playExercise} from "./scales";
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const scales = getWarmup([0,2,4,7,4,2,0]);
+const descend = getWarmup([4,3,2,1,0])
+
+const exerciseMap = {scales, descend};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+      exerciseKey: "descend",
+      exercise: descend
+    };
+  }
+
+  handleClickPlay= ()=> {
+    playExercise(this.state.exercise[this.state.index]);
+  }
+
+  handleClickPrev= ()=>{
+    let next = this.state.index - 1;
+    if(next < 0) next = 0;
+    this.setState({index: next});
+  }
+
+  handleClickNext= ()=> {
+    const next = this.state.index + 1;
+    this.setState({index: next});
+  }
+  
+  handleClickFirstNote= ()=> {
+    playExercise([this.state.exercise[this.state.index][0]])
+  }
+
+  handleChangeExercise= (e)=> {
+    const exerciseKey = e.target.value;
+    this.setState({
+      exerciseKey,
+      exercise: exerciseMap[exerciseKey],
+      index: 0
+    })
+  }
+
+  renderSelect=(exerciseKey)=>{
+    return (
+      <Select
+      labelId=""
+      id="demo-simple-select"
+      value={exerciseKey}
+      onChange={this.handleChangeExercise.bind(this)}
+    >
+      <MenuItem value={"descend"}>Descend</MenuItem>
+      <MenuItem value={"scales"}>Scales</MenuItem>
+    </Select>
+    );
+  }
+
+  render(){
+    const exerciseLabel = this.state.exercise[this.state.index].join(",");
+    return (
+      <div className="App">
+        {this.renderSelect(this.state.exerciseKey)}
+        <h2>{exerciseLabel}</h2>
+        <button onClick={this.handleClickPrev.bind(this)}>Prev</button>
+        <button onClick={this.handleClickPlay.bind(this)}>Play</button>
+        <button onClick={this.handleClickFirstNote.bind(this)}>First Note</button>
+        <button onClick={this.handleClickNext.bind(this)}>Next</button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/scales.js
@@ -0,0 +1,93 @@
+import * as Tone from 'tone';
+
+const scales = [
+    "C, D, E, F, G, A, B, C",
+    "C#, Eb, F, F#, Ab, Bb, C, C#",
+    "D, E, F#, G, A, B, C#, D",
+    "E, F#, Ab, A, B, C#, Eb, E",
+    "F, G, A, Bb, C, D, E, F",
+    "F#, Ab, Bb, B, C#, Eb, F, F#",
+    "G, A, B, C, D, E, F#, G",
+    "Ab, Bb, C, Db, Eb, F, G, Ab",
+    "A, B, C#, D, E, F#, Ab, A",
+    "Bb, C, D, Eb, F, G, A, Bb",
+    "B, C#, Eb, E, F#, Ab, Bb, B",
+]
+
+// const scales = [
+//     "Ab, Bb, C, Db, Eb, F, G, Ab",
+//     "A, B, C#, D, E, F#, G#, A",
+//     "Bb, C, D, Eb, F, G, A, Bb",
+//     "B, C#, D#, E, F#, G#, A#, B",
+//     "C, D, E, F, G, A, B, C",
+//     "Db, Eb, F, Gb, Ab, Bb, C, Db",
+//     "D, E, F#, G, A, B, C#, D",
+//     "E, F#, G#, A, B, C#, D#, E",
+//     "F, G, A, Bb, C, D, E, F",
+//     "F#, G#, A#, B, C#, D#, F, F#",
+//     "G, A, B, C, D, E, F#, G",
+// ]
+
+export const getWarmup = (exerciseIndexes)=> {
+    const splitNotes = scales.map((x)=> x.split(", "));
+    
+    const exerciseArrs = [];
+
+    let majorOctave = 4;
+    while(majorOctave < 7) {
+        splitNotes.forEach(scale=>{
+            let octscale = [];
+            let exercise = [];
+            let octave = majorOctave;
+            const Cscale = scale[0].includes("C");
+
+            scale.forEach((note, j)=>{
+                if(note.includes("C") && !(Cscale && (j==0 || j ==6)) && octave < majorOctave+1){
+                    octave++;
+                }
+                octscale.push(note+octave);
+            });
+            exerciseIndexes.forEach(exIn => {
+                exercise.push(octscale[exIn]);
+            });
+            exerciseArrs.push(exercise);
+            if(scale[0].includes("G")){
+                majorOctave++;
+            }
+        })
+    }
+    return exerciseArrs;
+};
+
+const playArray = (arr,sampler)=> {
+    let time = Tone.now();
+    arr.forEach(note=>{
+        // trigger the attack immediately
+        sampler.triggerAttack(note, time)
+        // wait one second before triggering the release
+        sampler.triggerRelease(time + .5)
+        time+=.5;
+    });
+   // setTimeout(()=> sampler.releaseAll(),5000);
+}
+
+export const playExercise = (arr) => {
+    console.log(arr);
+    const urls = {};
+    arr.forEach((note)=>{
+        urls[note] = encodeURIComponent(`${note}.mp3`);
+    });
+    // const sampler = new Tone.Sampler({
+    //     urls,
+    //     release: .5,
+    //     baseUrl: "https://raw.githubusercontent.com/stephaniecurrandev/pianonotes/master/",
+    //     onload: function() {
+    //         playArray(arr,sampler);
+    //     }
+    // }).toDestination();
+    const synth = new Tone.Synth().toDestination();
+    playArray(arr,synth);
+
+}
+
+
